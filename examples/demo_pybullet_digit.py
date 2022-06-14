7bded71d3e0074b1d7afeb7ad9324b97fae5b007
your_username <canaler@artc.a-star.edu.sg>
2022-06-14 15:48:49 +0800

    Added Single Visualization and Save Image interface

--- examples/demo_pybullet_digit.py
+++ examples/demo_pybullet_digit.py
@@ -4,8 +4,9 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
-
+import numpy as np
 import cv2
+import os
 import hydra
 import pybullet as p
 import pybulletX as px
@@ -13,6 +14,7 @@ import tacto  # Import TACTO
 
 log = logging.getLogger(__name__)
 
+image_resize = [400,400] #hopefully the x is larger than 100
 
 # Load the config YAML file from examples/conf/digit.yaml
 @hydra.main(config_path="conf", config_name="digit")
@@ -33,6 +35,7 @@ def main(cfg):
 
     # Add object to pybullet and tacto simulator
     obj = px.Body(**cfg.object)
+    print(cfg.object)
     digits.add_body(obj)
 
     # Create control panel to control the 6DoF pose of the object
@@ -45,11 +48,34 @@ def main(cfg):
     t.start()
 
     while True:
+        #Getting sensor data as a single array        
         color, depth = digits.render()
+        
+        #preparing the iarrays mages as an image, for coloured and depth image
+        colors = np.concatenate(color, axis=1)        
+        depths =np.concatenate(list(map(digits._depth_to_color, depth)), axis=1)
+       
+       #resizing the images if needed
+        depths = cv2.resize(depths, (400, 400), interpolation = cv2.INTER_AREA)
+        colors = cv2.resize(colors, (400, 400), interpolation = cv2.INTER_AREA)
+        #Showing the iamges
+        cv2.imshow("depths", cv2.cvtColor(depths, cv2.COLOR_RGB2BGR))
+        cv2.imshow("colors", cv2.cvtColor(colors, cv2.COLOR_RGB2BGR))
+        c = cv2.waitKey(2000)
+        print (os.getcwd())        
+        if (c == ord('s')): 
+            print("saved")
+            cv2.imwrite(os.getcwd()+'/contact_image_dataset/test_depths.jpeg',depths) 
+            cv2.imwrite(os.getcwd()+'/contact_image_dataset/test_colors.jpeg',colors) 
+
+
+    
+
         digits.updateGUI(color, depth)
 
     t.stop()
 
 
+        
 if __name__ == "__main__":
     main()
