701c50636ea0c5cb1a8bb1afb9ae1c7344dbd423
your_username <canaler@artc.a-star.edu.sg>
2022-07-04 18:19:56 +0800

    more functions

--- examples/demo_pybullet_digit_image_functions.py
+++ examples/demo_pybullet_digit_image_functions.py
@@ -4,13 +4,14 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
-import numpy as np
+from numpy import concatenate, mean, size, float64, float32, uint8
 import cv2
-import os
+import matplotlib.pyplot as plt
 import hydra
 import pybullet as p
 import pybulletX as px
 import tacto  # Import TACTO
+from math import floor
 
 log = logging.getLogger(__name__)
 
@@ -46,31 +47,62 @@ def main(cfg):
     # run p.stepSimulation in another thread
     t = px.utils.SimulationThread(real_time_factor=1.0)
     t.start()
-
     while True:
+        
         #Getting sensor data as a single array        
         color, depth = digits.render()
         
         #preparing the iarrays mages as an image, for coloured and depth image
-        colors = np.concatenate(color, axis=1)        
-        depths =np.concatenate(list(map(digits._depth_to_color, depth)), axis=1)
+        colors = concatenate(color, axis=1)        
+        depths =concatenate(list(map(digits._depth_to_color, depth)), axis=1)  
+                 
+        #img=cv2.cvtColor(depths,cv2.COLOR_BGR2RGB)
+        vectorized = colors.reshape((-1,3))
+        vectorized = float32(vectorized)
 
-        for i, rows in enumerate(depths):
-            for j, pixel in enumerate(rows):
-                if pixel[0] >= 200 and pixel[1] >= 200 and pixel[1] >= 200:
-                    print("[", i,j,"]", pixel)
-            
-            
-            
-        #print(type(color),np.size(color), color)
-        #Showing the iamges
-       # cv2.imshow("depths", cv2.cvtColor(depths, cv2.COLOR_RGB2BGR))
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        K = 5
+        attempts=20
+        ret,label,center=cv2.kmeans(vectorized,K,None,criteria,attempts,cv2.KMEANS_PP_CENTERS)
+        center = uint8(center)
+        res = center[label.flatten()]
+        result_image = res.reshape((colors.shape))
+        figure_size = 15
+        plt.figure(figsize=(figure_size,figure_size))
+        plt.subplot(1,2,1),plt.imshow(colors)
+        plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+        plt.subplot(1,2,2),plt.imshow(result_image)
+        plt.title('Segmented Image when K = %i' % K), plt.xticks([]), plt.yticks([])
+        plt.show()
+        
+        #depths = Compute_BW_Contact_Intensity(depths)           
+        #depths = cv2.resize(depths, (500,500), interpolation = cv2.INTER_AREA)
+
+        cv2.imshow("depths", cv2.cvtColor(colors, cv2.COLOR_RGB2BGR))
         #cv2.imshow("colors", cv2.cvtColor(colors, cv2.COLOR_RGB2BGR))
-        #c = cv2.waitKey(2000)    
+
 
         digits.updateGUI(color, depth)
 
     t.stop()
+
+def Compute_BW_Contact_Intensity(depths):
+    intense_contact =  [(i,j)  for i, rows in enumerate(depths) for j,pixel in enumerate(rows) if (pixel[0] >= 200)]
+    medium_contact = [(i,j)  for i, rows in enumerate(depths) for j,pixel in enumerate(rows) if (pixel[0] >= 100 and pixel[0] < 200)]
+    weak_contact = [(i,j)  for i, rows in enumerate(depths) for j,pixel in enumerate(rows) if (pixel[0] >= 50 and  pixel[0] < 100)]
+        
+        #print("\n\nNumber of intense pixels: ", len(intense_contact), "\nNumber of medium pixels: ", len(medium_contact), "\nNumber of weak pixels: ", len(weak_contact))
+    if (intense_contact and medium_contact and weak_contact):
+        intense_contact_center = mean(intense_contact, axis=0, dtype=float64)
+        medium_contact_center = mean(medium_contact, axis=0, dtype=float64)
+        weak_contact_center = mean(weak_contact, axis=0, dtype=float64)
+
+        print(intense_contact_center, medium_contact_center, weak_contact_center)
+
+        depths = cv2.circle(depths, (floor(intense_contact_center[1]),floor(intense_contact_center[0])), 0, [255, 0, 0], 2)
+        depths = cv2.circle(depths, (floor(medium_contact_center[1]),floor(medium_contact_center[0])), 0, [200, 0, 200], 2)
+        depths = cv2.circle(depths, (floor(weak_contact_center[1]),floor(weak_contact_center[0])), 0, [0,0,2500], 2)
+    return depths
 
 
         
